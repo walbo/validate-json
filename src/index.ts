55b76df6f614827bd885e51daad2542c74062dd9
Petter Walbø Johnsgård <petter@dekode.no>
2022-02-19 16:30:49 +0100

    Add strict option (#104)
    * Add strict option
    
    * Build

--- src/index.ts
+++ src/index.ts
@@ -58,8 +58,9 @@ async function run() {
 		const allowMatchingProperties = isTrueSet(
 			getInput('allow-matching-properties'),
 		);
+		const strict = isTrueSet(getInput('strict'));
 
-		const ajv = getAjv(schemaVersion, { allowMatchingProperties });
+		const ajv = getAjv(schemaVersion, { allowMatchingProperties, strict });
 
 		if (!ajv) {
 			throw new Error('Unsupported schema');
